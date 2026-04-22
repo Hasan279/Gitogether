c99b35622e69e21472eaf82874265dd8ac1bc354
Areeb Saeed <mareebsaeed@gmail.com>
2026-04-22 14:49:10 +0500

    rating functions

---
+++ models/rating.py
@@ -0,0 +1,104 @@
+from models.db import get_connection
+
+
+def create_rating(match_id, rater_id, rated_id, score, review):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("""
+                insert into ratings (match_id, rater_id, rated_id, score, review)
+                values (%s, %s, %s, %s, %s)
+                RETURNING rating_id
+    """, (match_id, rater_id, rated_id, score, review))
+    
+    rating_id = cur.fetchone()[0]
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    return rating_id
+
+def get_rating_by_id(rating_id):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("""
+                select * from ratings
+                where rating_id = %s
+    """, (rating_id,))
+    
+    rating_details = cur.fetchall()
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    return rating_details
+
+def get_ratings_by_developer(developer_id):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("""
+                select * from ratings
+                where rated_id = %s
+    """, (developer_id,))
+
+    details = cur.fetchall()
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return details
+
+def get_average_rating(developer_id):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("""
+                select avg(score) from ratings
+                where rated_id = %s
+    """, (developer_id,))
+
+    average_rating = cur.fetchone()[0]
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return average_rating
+
+def check_existing_rating(match_id, rater_id):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("""
+                select * from ratings
+                where match_id = %s and rater_id = %s
+    """, (match_id, rater_id))
+
+    rating = cur.fetchone()
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return rating
+
+def get_ratings_by_match(match_id):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("""
+                select * from ratings
+                where match_id = %s
+    """, (match_id,))
+
+    rating = cur.fetchall()
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return rating
