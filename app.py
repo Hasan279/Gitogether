2b7cb757870428d572213ab9193905121b18ddcc
Hasan279 <hasanwaseem279@gmail.com>
2026-04-22 15:30:44 +0500

    admin panel

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for
+from flask import Flask, redirect, url_for
 from config import SECRET_KEY, DEBUG
 
 app = Flask(__name__)
@@ -8,15 +8,16 @@ from controllers import auth, dashboard, projects, developers, requests, matches
 
 app.register_blueprint(auth.bp)
 app.register_blueprint(dashboard.bp)
-# app.register_blueprint(projects.bp)
-# app.register_blueprint(developers.bp)
-# app.register_blueprint(requests.bp)
-# app.register_blueprint(matches.bp)
-# app.register_blueprint(ratings.bp)
-# app.register_blueprint(admin.bp)
+app.register_blueprint(projects.bp)
+app.register_blueprint(developers.bp)
+app.register_blueprint(requests.bp)
+app.register_blueprint(matches.bp)
+app.register_blueprint(ratings.bp)
+app.register_blueprint(admin.bp)
 
 @app.route('/')
 def home():
     return redirect(url_for('auth.login'))
+
 if __name__ == "__main__":
     app.run(debug=DEBUG)
